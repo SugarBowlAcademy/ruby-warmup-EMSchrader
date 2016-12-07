9f1de3e84681f03ad3319fd0b6f32eacf1e6fca2
EMSchrader <eleeces@icloud.com>
2016-12-07 15:31:47 -0800

    Create warmup.rb

---
+++ warmup.rb
@@ -0,0 +1,14 @@
+pet names = [ "pow pow", "scout", "kira", "marley", "spots"] 
+
+petnames.each do |names|
+puts names 
+end 
+
+for names in petnames
+puts names
+end 
+
+begin 
+which loop do you like better?
+I like the second loop better because for me its easier to use. 
+end 
